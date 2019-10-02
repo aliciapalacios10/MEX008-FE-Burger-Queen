e6e9c27eff6f41c4bb6bb7d371d716f1109f626e
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-10-02 13:39:37 -0500

    Updates

---
+++ precache-manifest.6bf2dea0d5710fd5f5dfc9e28e54b74a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc3788eb3fd1dfeba2a29769f6d44d4",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "66ec6d5ac6faced9446c",
+    "url": "/MEX008-FE-Burger-Queen/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "47f3fafb4f42a3a7d800",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.8954708b.chunk.css"
+  },
+  {
+    "revision": "66ec6d5ac6faced9446c",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.99ad8c1a.chunk.js"
+  },
+  {
+    "revision": "47f3fafb4f42a3a7d800",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.e973c4d1.chunk.js"
+  },
+  {
+    "revision": "c08034477bcddfd21ef2",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.99333e17.js"
+  },
+  {
+    "revision": "733372fdb71ccdc1485852ffc6e790b0",
+    "url": "/MEX008-FE-Burger-Queen/static/media/PB.733372fd.jpeg"
+  },
+  {
+    "revision": "afd5dee3dda904bb63815860fe6c9b72",
+    "url": "/MEX008-FE-Burger-Queen/static/media/imagebackground.afd5dee3.png"
+  },
+  {
+    "revision": "8933daf5f6f9935079b85556bee1f661",
+    "url": "/MEX008-FE-Burger-Queen/static/media/slate.8933daf5.jpeg"
+  },
+  {
+    "revision": "ed0987fe7172ac3ef9ab5df815c984e1",
+    "url": "/MEX008-FE-Burger-Queen/static/media/vbnapa.ed0987fe.jpeg"
+  }
+]);
